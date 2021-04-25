bf18adf057ae89bb7e7a475779a1b81b404a1920
nitincs17 <37626694+nitincs17@users.noreply.github.com>
2021-04-25 11:43:40 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def predict():
 
     
 
-    return render_template('index.html', prediction_text='Predicted Species is  $ {}'.format(round(prediction[0][0])))
+    return render_template('index.html', prediction_text='Predicted Species is  {}'.format(prediction[0]))
 
 
 if __name__ == "__main__":
